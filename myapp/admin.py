879d1312ffbec46567ae882f4af02501054641e8
sampada57 <sampadha.kharel@gmail.com>
2024-01-19 14:35:00 +0545

    created superuser and registered models

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Product , Customer , Order
 
 # Register your models here.
+#class ProductAdmin(admin.ModelAdmin):
+    #list_display = ('field1', 'field2', 'field3')
+
+admin.site.register(Product)
+admin.site.register(Customer)
+admin.site.register(Order)
