cdc55f70d99d6d51acddbc598088f59584bebcdf
sampada57 <sampadha.kharel@gmail.com>
2024-01-21 12:41:22 +0545

    chapter three

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import homePageView
+from .views import HomePageView
+from .views import AboutPageView
 
 urlpatterns = [
-path("", homePageView, name="home"),
+#path("", homePageView, name="home"),
+path("", HomePageView.as_view(), name="home"),
+path("about/",AboutPageView.as_view(),name="about")
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
+
 
 # Create your views here.
 def homePageView(request):
     return HttpResponse("Hello, World!")
+
+class HomePageView(TemplateView):
+    template_name = "demo.html"
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
